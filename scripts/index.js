7753f21de06b082f03c175b1bc922ebfbc4e7172
Briscott08 <briscott08@gmail.com>
2024-01-31 20:56:14 -0500

    model edits saves and closes using javascript

--- scripts/index.js
+++ scripts/index.js
@@ -30,18 +30,74 @@ const initialCards = [
   },
 ];
 
-// Elements
+// ========= //
+// Elements  //
+// ========= //
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileEditButtonClose = document.querySelector(
   "#profile-edit-close-btn"
 );
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+const cardListEl = document.querySelector(".cards__list");
+
+// =============== //
+//    Functions    //
+// =============== //
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitleEl = cardElement.querySelector(".card__name");
+  const cardImageEl = cardElement.querySelector(".card__image");
+
+  //set the image alt text to the name field of the object
+
+  cardTitleEl.textContent = cardData.name;
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+
+  return cardElement;
+}
+
+// =============== //
+// Event Handlers  //
+// =============== //
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+
+// ================ //
+// Event Listeners  //
+// ================ //
 
 profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal_opened");
 });
 
-profileEditButtonClose.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
+profileEditButtonClose.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
 });
